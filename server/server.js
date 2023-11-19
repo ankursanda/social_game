babb62cf13473c67882e2e88371551723b2816c7
Ankur Sanda <sanda1511.baharagora@gmail.com>
2023-11-19 20:08:33 +0530

    backend successfully connected with the front end

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const birds = require("./birds");
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
+
+app.use(cors());
 app.use('/birds',birds);
 
 app.get('/', (req, res, next)=>{
-    res.send("Sending this response in the home page")
+    data = {name: 'atom'}
+    res.send(JSON.stringify(data))
 })
 
 app.get('/service/:id', (req, res, next)=> {
--- ui/client/src/App.js
+++ ui/client/src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const App = () => {
+  const [myName, setMyName] = useState(null);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/", {
+        method: 'GET',
+        mode: 'cors'
+      });
 
-function App() {
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        setMyName(jsonResponse.name);
+      } else {
+        console.log("Failed to gain access to the API address");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs only once, like componentDidMount
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World, my name is {myName}</h1>
     </div>
   );
 }
